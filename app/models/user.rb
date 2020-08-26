538827ef14104cb7feb24469c7c96db5dde0759b
jun.katami <tami708j_airuy@yahoo.co.jp>
2020-08-26 19:48:16 +0900

    users_login_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :account_id, :account_name, presence: true
-  has_many :posts
+  has_many :posts, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -8,4 +8,61 @@ RSpec.describe "Posts", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /new" do
+    context "ログイン済みのユーザー" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      it "正常なレスポンスを返すこと" do
+        sign_in @user
+        get new_post_path
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "ログインしていない" do
+      it "302レスポンスを返すこと" do
+        get new_post_path
+        expect(response).to have_http_status(302)
+      end
+
+      it "ログイン画面へリダイレクトすること" do
+        get new_post_path
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+
+  describe "POST /create" do
+    context "ログイン済みのユーザー" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      it "投稿を追加できること" do
+        post_params = FactoryBot.attributes_for(:post)
+        sign_in @user
+        expect {
+          post posts_path, params: { post: post_params }
+        }.to change(@user.posts, :count).by(1)
+      end
+    end
+
+    context "ログインしていない" do
+      it "302レスポンスを返すこと" do
+        post_params = FactoryBot.attributes_for(:post)
+        post posts_path, params: { post: post_params }
+        expect(response).to have_http_status(302)
+      end
+
+      it "ログイン画面へリダイレクトすること" do
+        post_params = FactoryBot.attributes_for(:post)
+        post posts_path, params: { post: post_params }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
 end
